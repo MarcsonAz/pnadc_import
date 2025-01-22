050aa3d3f888ad6caf9c8cdec800e378da11906b
Marcson de Azevedo Araújo <B15048941705@ipea.gov.br>
2025-01-21 22:13:08 -0300

    ajuste leitura arquivo 01

--- 01_organizar_parquet.R
+++ 01_organizar_parquet.R
@@ -5,7 +5,7 @@
 require(pacman)
 
 p_load(
-  c('PNADcIBGE','dplyr','fs'),
+  c('PNADcIBGE','dplyr','fs','stringr'),
   character.only = TRUE
 )
 
@@ -13,14 +13,15 @@ source('./funcoes/funcoes_apoio.R')
 
 #### ANUAL ####
 
-diretorio_microdados_v1 = "./../Anual/Microdados/Visita/Visita_1/"
-diretorio_microdados_v5 = "./../Anual/Microdados/Visita/Visita_5/"
-
 #diretorio_microdados_v1
 ## pegar dados da Pnad Continua de 2022, visita 1
 
-data_path = encontra_pnadc(ano=2022,caminho=paste0(diretorio_microdados_v1))
-input_path = encontra_input_pnadc(ano=2022,caminho=paste0(diretorio_microdados_v1))
+ANO = 2023
+VISITA = 1
+
+
+data_path = encontra_pnadc(ano=ANO,caminho=diretorio_microdados(VISITA))
+input_path = encontra_input_pnadc(ano=ANO,caminho=diretorio_microdados(VISITA))
 
 pnadc.df <- read_pnadc(microdata=data_path, 
                        input_txt=input_path, 
@@ -34,7 +35,7 @@ diretorio_microdados_parquet= "./../parquet/"
 ano = 2022
 visita = 1
 nome_arquivo = paste0(
-  diretorio_microdados_parquet,"arquivo_microdados_",2022,"_anual_v",visita,"_teste.parquet"
+  diretorio_microdados_parquet,"arquivo_microdados_",ANO,"_anual_v",VISITA,"_teste.parquet"
 )
 arrow::write_parquet(pnadc.df,nome_arquivo)
 
@@ -88,7 +89,50 @@ arrow::write_parquet(pnadc.df,nome_arquivo)
 
 
 
+#### nova proposta
 
+
+library(readr)
+library(arrow)
+library(dplyr)
+
+process_zip_files <- function(dir1, dir2) {
+  # Lista todos os arquivos zip na pasta dir1
+  arquivos_compacto <- listar_arq_pnadc_tri_compactos()
+  
+  for (zip_file in arquivos_compacto) {
+    # Cria uma pasta temporária para descompactar os arquivos
+    temp_dir <- tempdir()
+    unzip(zip_file, exdir = temp_dir)
+    
+    # Lista todos os arquivos descompactados
+    extracted_files <- list.files(temp_dir, full.names = TRUE)
+    
+    for (file in extracted_files) {
+      # Lê a planilha usando readr
+      data <- read_csv(file)
+      
+      # Seleciona as colunas col1, col2 e col3
+      selected_data <- data %>% select(col1, col2, col3)
+      
+      # Define o nome do arquivo parquet
+      parquet_file <- file.path(dir2, paste0(tools::file_path_sans_ext(basename(file)), ".parquet"))
+      
+      # Salva o dataframe em formato parquet
+      write_parquet(selected_data, parquet_file)
+    }
+  }
+}
+
+# Exemplo de uso
+dir1 <- "caminho/para/dir1"
+dir2 <- "caminho/para/dir2"
+process_zip_files(dir1, dir2)
+
+
+#Certifique-se de ajustar os caminhos `dir1` e `dir2` para os diretórios corretos onde os arquivos zip estão localizados e onde você deseja salvar os arquivos parquet.
+
+#Se precisar de mais alguma coisa ou tiver alguma dúvida, estou à disposição!
 
 
 
--- funcoes/funcoes_apoio.R
+++ funcoes/funcoes_apoio.R
@@ -15,10 +15,17 @@ encontra_pnadc <- function(ano,caminho = ""){
   stopifnot("ano deve ser numérico"=is.numeric(ano))
   stopifnot("caminho deve ser texto"=is.character(caminho))
   
-  caminho_final <-list.files(paste0(caminho,"Dados"), 
+  caminho_final <- list.files(paste0(caminho,"Dados"), 
                    pattern=paste0("*PNADC_",ano), full.names=TRUE)
   
-  return(caminho_final)
+  if(length(caminho_final)>1){
+    i_arquivo_recente = which.min(
+      order(
+        file_info(
+          caminho_final)$modification_time, 
+        decreasing = TRUE))
+    }
+  return(caminho_final[i_arquivo_recente])
 }
 
 
@@ -38,8 +45,14 @@ encontra_input_pnadc <- function(ano,caminho = ""){
   
   caminho_final <-list.files(paste0(caminho,"Documentacao"), 
                              pattern=paste0("*PNADC_",ano), full.names=TRUE)
-  
-  return(caminho_final)
+  if(length(caminho_final)>1){
+    i_arquivo_recente = which.min(
+      order(
+        file_info(
+          caminho_final)$modification_time, 
+        decreasing = TRUE))
+  }
+  return(caminho_final[i_arquivo_recente])
 }
 
 
@@ -53,17 +66,50 @@ encontra_input_pnadc <- function(ano,caminho = ""){
 # funcao final de atualizar dados de tri
 
 # preciso: 
-# - lista de arquivos ja em parquet
-# - lista de arquivos compactados
-# - filtrar dado a comparação das lista acima
+# - lista de arquivos ja em parquet (OK)
+# - lista de arquivos compactados (OK)
+# --- pegar anos com nome de pasta (OK)
+# - filtrar dado a comparação das lista acima ()
 # - carregar os necessários no R, com variáveis selecionadas e 
 # salvar em parquet.
 
-listar_arq_pnadc_tri_compactos <- function(caminho = ""){
+listar_arq_pnadc_tri_compactos <- function(
+    caminho = "./../Trimestral/Microdados/"){
+  
   stopifnot("caminho deve ser texto"=is.character(caminho))
   
-  caminho_final <-list.files(paste0(caminho,"Dados"), 
-                             pattern=paste0("*PNADC_",ano), full.names=TRUE)
+  arquivos_compacto <- list.dirs(caminho) %>% 
+    filtrar_dir_anos() %>% 
+    list.files(full.names = TRUE,recursive = TRUE)
+  
+  return(arquivos_compacto)
+}
+
+listar_arq_pnadc_tri_parquet <- function(
+    caminho = "./../parquet/"){
+  
+  stopifnot("caminho deve ser texto"=is.character(caminho))
+  
+  arquivos_parquet <- list.files(caminho,full.names = TRUE,recursive = TRUE)
+  
+  return(arquivos_parquet)
+}
+
+
+parquet_antigo <- function(
+    caminho = "./../parquet/"){
+  
+  stopifnot("caminho deve ser texto"=is.character(caminho))
+  
+    arquivos_parquet <- listar_arq_pnadc_tri_parquet()
+    arquivos_compacto <- listar_arq_pnadc_tri_compactos()
+    
+    
+  
+  return(arquivos_parquet)
+}
+
+
 
 
 
@@ -82,8 +128,31 @@ lista_arquivos_pnadc_tri <- function(ano = "todos",caminho = "", tri=NULL){
 
 
 
+filtrar_dir_anos <- function(dir_principal) {
+  
+  pattern <- "/20\\d{2}$"
+  dir_anos <- dir_principal[grepl(pattern, dir_principal)]
+  
+  return(dir_anos)
+}
 
 
+filtrar_dir_anos(caminho_final)
+
+
+
+
+diretorio_microdados <- function(n_visita){
+  dir_apoio = "./../Anual/Microdados/Visita/Visita_"
+  
+  if(n_visita == 1){
+    return(paste0(dir_apoio,'1/'))
+  }
+  
+  if(n_visita == 5){
+    return(paste0(dir_apoio,'5/'))
+  }
+}
 
 
 
